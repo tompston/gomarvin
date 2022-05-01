499be59742519ab36b2ff576f496f6f5c2b65592
tompston <tompston@protonmail.com>
2022-05-01 17:23:07 +0300

    refactoring comments, fetch examples

--- test/fetch/src/app.tsx
+++ test/fetch/src/app.tsx
@@ -1,128 +1,73 @@
 import * as F from "../../../gomarvin.gen";
+import { CommentEndpoints } from "../../../gomarvin.gen";
 import { Test, FetchCreateUserEndpoint } from "./lib/tests";
 
 export function App() {
-  // https://developer.mozilla.org/en-US/docs/Web/API/Headers/Headers
-  function testHeaders(custom_headers: object = {}) {
-    let headers = F.createHeaders(custom_headers);
-    for (let x of headers.entries()) {
-      console.log(x[0] + ": " + x[1]);
-    }
-  }
-
-  let customheadersFetch = () =>
-    testHeaders({
-      "new-header": "this-is-a-new-header",
-    });
-
-  /**
-   * optional interface used in the fetch request with optional parameters
-   * @param {RequestInit} [options]
-   * If default fetch options need to be edited, provide a custom options object
-   * @param {string} [append_url]
-   * extend the url with custom params (like "?name=jim")
-   */
-  interface OptionalFetchParams {
-    options?: RequestInit;
-    append_url?: string;
-  }
-
-  async function RandomEndpointWithInterface(
-    opt?: OptionalFetchParams
-  ): Promise<Response> {
-    const appended_url = opt?.append_url ? opt?.append_url : "";
-    const url = `${F.API.url}/user${appended_url}`;
-    console.log(url);
-
-    console.log("options -->" + opt?.options);
-    if (opt?.options) {
-      return await fetch(url, opt?.options);
-    } else {
-      return await fetch(url, {
-        method: "GET",
-        headers: F.API.init_headers,
-      });
-    }
-  }
-
-  /**
-   * @param {RequestInit} [options]
-   * If default options need to be edited, provide a custom options object
-   */
-  async function RandomEndpoint(
-    options?: RequestInit,
-    append_url?: string
-  ): Promise<Response> {
-    const appended_url = append_url ? append_url : "";
-    const url = `${F.API.url}/user${appended_url}`;
-    console.log(url);
-
-    // console.log("options -->" + options);
-    if (options) {
-      return await fetch(url, options);
-    } else {
-      return await fetch(url, {
-        method: "GET",
-        headers: F.API.init_headers,
-      });
-    }
-  }
-
-  // ----- Functioons that don't have optional named params
-  async function randomEndpointDefaultOptions() {
-    const res = await RandomEndpoint();
+  // fetch GetUserById endpoint with changed options
+  async function FetchGetUsersById() {
+    const res = await F.GetUserById(10);
     console.log(res);
   }
-  async function randomEndpointCustomOptions() {
-    const res = await RandomEndpoint({ method: "get" });
+
+  // append optional string to the existing endpoint url
+  async function FetchEndpointWithAppendedUrl() {
+    const res = await F.GetUserById(10, { append_url: "?name=jim" });
     console.log(res);
   }
-  async function IncorrectOptionsTest() {
+
+  // define custom options for the fetch request
+  async function FetchEndpointWithCustomOptions() {
     const res = await F.GetUserById(10, { options: { method: "POST" } });
     console.log(res);
   }
 
-  // ----- Functioons that have optional named params
-  async function randomEndpointCustomOptionsInterface() {
-    const res = await RandomEndpointWithInterface({
-      // append_url: "/qwe",
+  // define custom options for the fetch request
+  async function FetchWithAppendedUrlAndCustomOptions() {
+    const res = await F.GetUserById(10, {
       options: { method: "DELETE" },
+      append_url: "?name=jim",
     });
-    // console.log(res);
+    console.log(res);
+  }
+
+  // Fetch a singe endpoint from the Comment module
+  async function FetchCommentById() {
+    const res = await CommentEndpoints.GetComment(20);
+    console.log(res);
   }
 
   return (
     <>
-      <div class="flex-center">
+      <div class="flex-center test-btn-grid">
+        {/* -- Imported funcs */}
         <div onClick={Test} class="test-btn">
           Test
         </div>
-      </div>
-
-      <div>
         <div onClick={FetchCreateUserEndpoint} class="test-btn">
           FetchCreateUserEndpoint
         </div>
-        <div onClick={testHeaders} class="test-btn">
-          testHeaders
+        {/* -- local funcs */}
+        <div onClick={FetchGetUsersById} class="test-btn">
+          FetchGetUsersById
         </div>
-        <div onClick={customheadersFetch} class="test-btn">
-          customheadersFetch
+        <div onClick={FetchEndpointWithAppendedUrl} class="test-btn">
+          FetchEndpointWithAppendedUrl
         </div>
-        <div onClick={randomEndpointDefaultOptions} class="test-btn">
-          randomEndpointDefaultOptions
+        <div
+          onClick={FetchEndpointWithCustomOptions}
+          class="test-btn should-return-err"
+        >
+          FetchEndpointWithCustomOptions
         </div>
-        <div onClick={randomEndpointCustomOptions} class="test-btn">
-          randomEndpointCustomOptions
+        <div
+          onClick={FetchWithAppendedUrlAndCustomOptions}
+          class="test-btn should-return-err"
+        >
+          FetchWithAppendedUrlAndCustomOptions
         </div>
-        <div onClick={IncorrectOptionsTest} class="test-btn">
-          IncorrectOptionsTest
+        <div onClick={FetchCommentById} class="test-btn">
+          FetchCommentById
         </div>
-        <div onClick={randomEndpointCustomOptionsInterface} class="test-btn">
-          randomEndpointCustomOptionsInterface
-        </div>
-
-        {/* randomEndpointCustomOptionsqq */}
       </div>
     </>
   );
