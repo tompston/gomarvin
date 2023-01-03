a1fa8c8d968f80c8f008fc4d32aefbefda0fbda7
tompston <tompston@protonmail.com>
2023-01-03 20:09:31 +0200

    cleaning up dirs

--- test/generate-project.sh
+++ test/generate-project.sh
@@ -2,7 +2,7 @@
 # chmod +x ./test/generate-project.sh
 
 go run main.go -dangerous-regen="true" \
-    -config="./examples/v0.4.0/gomarvin-chi_with_modules.json" \
+    -config="./examples/v0.5.0/gomarvin-chi_with_modules.json" \
     generate
 
 cd chi_with_modules
