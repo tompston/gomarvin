ebc08ae6435459dffc56993975c62048f38ad79f
tompston <tompston@protonmail.com>
2023-01-03 19:54:01 +0200

    generate lib/utils dir for placeholder function

--- marvin/paths.go
+++ marvin/paths.go
@@ -5,6 +5,7 @@ var init_project_dirs = [...]string{
 	"/settings/database/",
 	"/lib/response/",
 	"/lib/validate/",
+	"/lib/utils/",
 	"/app/",
 	"/public/",
 }
@@ -20,6 +21,7 @@ var init_project_templates = []Template{
 	{template_path: "templates/init/database.go.tmpl", output_dir: "/settings/database/"},
 	{template_path: "templates/init/response.go.tmpl", output_dir: "/lib/response/"},
 	{template_path: "templates/init/validate.go.tmpl", output_dir: "/lib/validate/"},
+	{template_path: "templates/init/utils.go.tmpl", output_dir: "/lib/utils/"},
 	{template_path: "templates/init/router.go.tmpl", output_dir: "/app/"},
 
 	// server.go.tmpl
---
+++ marvin/templates/init/utils.go.tmpl
@@ -0,0 +1,9 @@
+package utils
+
+// Placeholder function to be replaced after codegen.
+// Usually replaced with the function that returns the
+// data from the database, if everything goes ok.
+func PlaceholderFunction() (struct{}, error) {
+	var placeholder struct{}
+	return placeholder, nil
+}
