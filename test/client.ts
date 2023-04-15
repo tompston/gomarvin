c66bf912c59f1995c2589678a6a3cc15012e688d
tompston <tompston@protonmail.com>
2023-04-15 17:14:36 +0300

    writing api tests

--- test/client.ts
+++ test/client.ts
@@ -8,6 +8,14 @@ import {
 
 // deno test --allow-net ./test/client.ts
 
+const ok_response = { status: 200, message: "", data: {} };
+const ok_response_with_links = {
+  status: 200,
+  message: "",
+  data: {},
+  links: {},
+};
+
 const client = gomarvin.defaultClient;
 
 // @ts-ignore
@@ -50,19 +58,74 @@ Deno.test(
   "Module generate with the crud option works as expected",
   async () => {
     const crud = gomarvin.CrudEndpoints;
-    // @ts-ignore
-    const data = await (await gomarvin.CreateUser(client, null)).json();
-    // console.log(JSON.stringify(data, null));
-    assertEquals(data, {
-      status: 400,
-      message: "Payload validation failed!",
-      data: {
-        errors: [
-          { failed_field: "Username", message: "Username is required" },
-          { failed_field: "Email", message: "Email is required" },
-          { failed_field: "Password", message: "Password is required" },
-        ],
-      },
-    });
+    // get_many
+    const get_many = await (await crud.GetCruds(client)).json();
+    assertEquals(get_many, ok_response_with_links);
+    // get_one
+    const get_one = await (await crud.GetCrud(client, 1)).json();
+    assertEquals(get_one, ok_response);
+    // create
+    const create = await (await crud.CreateCrud(client)).json();
+    assertEquals(create, ok_response);
+    // update
+    const update = await (await crud.UpdateCrud(client)).json();
+    assertEquals(update, ok_response);
+    // delete
+    const del = await (await crud.DeleteCrud(client)).json();
+    assertEquals(del, ok_response);
   }
 );
+
+// @ts-ignore
+Deno.test("Create user with valid data", async () => {
+  const data = await (
+    await gomarvin.CreateUser(client, {
+      username: "test",
+      email: "tom@gmail.com",
+      password: "test123123",
+    })
+  ).json();
+  assertEquals(data, ok_response);
+});
+
+// @ts-ignore
+Deno.test("Create user fails if incorrect email is provided", async () => {
+  const data = await (
+    await gomarvin.CreateUser(client, {
+      username: "test",
+      email: "tom@gmail",
+      password: "test123123",
+    })
+  ).json();
+  console.log(JSON.stringify(data, null));
+  assertEquals(data, {
+    status: 400,
+    message: "Payload validation failed!",
+    data: {
+      errors: [
+        {
+          failed_field: "Email",
+          message: "Email must be a valid email address",
+        },
+      ],
+    },
+  });
+});
+
+// @ts-ignore
+Deno.test("Create user fails no body is provided", async () => {
+  const data = await // @ts-ignore
+  (await gomarvin.CreateUser(client, null)).json();
+  console.log(JSON.stringify(data, null));
+  assertEquals(data, {
+    status: 400,
+    message: "Payload validation failed!",
+    data: {
+      errors: [
+        { failed_field: "Username", message: "Username is required" },
+        { failed_field: "Email", message: "Email is required" },
+        { failed_field: "Password", message: "Password is required" },
+      ],
+    },
+  });
+});
