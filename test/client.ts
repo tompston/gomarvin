3730849c22913cb234c4bb1af30a2d5607a98ad0
tompston <tompston@protonmail.com>
2023-04-15 18:26:09 +0300

    porting postman test to typescript

--- test/client.ts
+++ test/client.ts
@@ -8,8 +8,12 @@ import {
 
 // deno test --allow-net ./test/client.ts
 
-const ok_response = { status: 200, message: "", data: {} };
-const ok_response_with_links = {
+// @ts-ignore
+// Deno.test("name of the test", async () => {});
+
+const _t = gomarvin.TestEndpoints;
+const response_ok = { status: 200, message: "", data: {} };
+const response_ok_with_links = {
   status: 200,
   message: "",
   data: {},
@@ -19,18 +23,14 @@ const ok_response_with_links = {
 const client = gomarvin.defaultClient;
 
 // @ts-ignore
-Deno.test(
-  "api return empty links object when pagination setting is true",
-  async () => {
-    const data = await (await gomarvin.GetUsers(client)).json();
-    assertEquals(data, {
-      status: 200,
-      message: "",
-      data: {},
-      links: {},
-    });
-  }
-);
+Deno.test("Home view works", async () => {
+  const data = await (await fetch("http://localhost:4444/")).json();
+  assertEquals(data, {
+    status: 200,
+    message: "Hello There!",
+    data: null,
+  });
+});
 
 // @ts-ignore
 Deno.test(
@@ -38,7 +38,6 @@ Deno.test(
   async () => {
     // @ts-ignore
     const data = await (await gomarvin.CreateUser(client, null)).json();
-    // console.log(JSON.stringify(data, null));
     assertEquals(data, {
       status: 400,
       message: "Payload validation failed!",
@@ -60,24 +59,24 @@ Deno.test(
     const crud = gomarvin.CrudEndpoints;
     // get_many
     const get_many = await (await crud.GetCruds(client)).json();
-    assertEquals(get_many, ok_response_with_links);
+    assertEquals(get_many, response_ok_with_links);
     // get_one
     const get_one = await (await crud.GetCrud(client, 1)).json();
-    assertEquals(get_one, ok_response);
+    assertEquals(get_one, response_ok);
     // create
     const create = await (await crud.CreateCrud(client)).json();
-    assertEquals(create, ok_response);
+    assertEquals(create, response_ok);
     // update
     const update = await (await crud.UpdateCrud(client)).json();
-    assertEquals(update, ok_response);
+    assertEquals(update, response_ok);
     // delete
     const del = await (await crud.DeleteCrud(client)).json();
-    assertEquals(del, ok_response);
+    assertEquals(del, response_ok);
   }
 );
 
 // @ts-ignore
-Deno.test("Create user with valid data", async () => {
+Deno.test("Create user with valid data returns status ok", async () => {
   const data = await (
     await gomarvin.CreateUser(client, {
       username: "test",
@@ -85,7 +84,7 @@ Deno.test("Create user with valid data", async () => {
       password: "test123123",
     })
   ).json();
-  assertEquals(data, ok_response);
+  assertEquals(data, response_ok);
 });
 
 // @ts-ignore
@@ -97,7 +96,6 @@ Deno.test("Create user fails if incorrect email is provided", async () => {
       password: "test123123",
     })
   ).json();
-  console.log(JSON.stringify(data, null));
   assertEquals(data, {
     status: 400,
     message: "Payload validation failed!",
@@ -116,7 +114,6 @@ Deno.test("Create user fails if incorrect email is provided", async () => {
 Deno.test("Create user fails no body is provided", async () => {
   const data = await // @ts-ignore
   (await gomarvin.CreateUser(client, null)).json();
-  console.log(JSON.stringify(data, null));
   assertEquals(data, {
     status: 400,
     message: "Payload validation failed!",
@@ -128,4 +125,109 @@ Deno.test("Create user fails no body is provided", async () => {
       ],
     },
   });
+});
+
+// @ts-ignore
+Deno.test(
+  "Create user fails if password does not pass validations",
+  async () => {
+    const data = await (
+      await gomarvin.CreateUser(client, {
+        username: "test",
+        email: "qwe@qwe.com",
+        password: "short",
+      })
+    ).json();
+    assertEquals(data, {
+      status: 400,
+      message: "Payload validation failed!",
+      data: {
+        errors: [
+          {
+            failed_field: "Password",
+            message: "Password should be at least 10 characters long",
+          },
+        ],
+      },
+    });
+  }
+);
+
+// @ts-ignore
+Deno.test("Url With string param", async () => {
+  const data = await (await _t.UrlWithString(client, "qwe")).json();
+  assertEquals(data, response_ok);
+});
+
+// @ts-ignore
+Deno.test("Url With int param", async () => {
+  const data = await (await _t.UrlWithInt(client, 123)).json();
+  assertEquals(data, response_ok);
+});
+
+// @ts-ignore
+Deno.test("UndefinedValidation field returns correct err", async () => {
+  const data = await (
+    await _t.UndefinedValidation(client, { undefined_validation_field: "" })
+  ).json();
+  assertEquals(data, {
+    status: 400,
+    message: "Payload validation failed!",
+    data: {
+      errors: [
+        {
+          failed_field: "UndefinedValidationField",
+          message: "UndefinedValidationField is required",
+        },
+      ],
+    },
+  });
+});
+
+// @ts-ignore
+Deno.test("Min validation fails when needed", async () => {
+  // Min validation fails
+  const t4 = await (await _t.MinValidation(client, { min_ten: "qwe" })).json();
+  assertEquals(t4, {
+    status: 400,
+    message: "Payload validation failed!",
+    data: {
+      errors: [
+        {
+          failed_field: "MinTen",
+          message: "MinTen should be at least 10 characters long",
+        },
+      ],
+    },
+  });
+
+  // Min validation passes
+  const t5 = await (
+    await _t.MinValidation(client, { min_ten: "qweqweqweqweqweqweqweqwe" })
+  ).json();
+  assertEquals(t5, response_ok);
+});
+
+// @ts-ignore
+Deno.test("Max validation fails when needed", async () => {
+  // Max validation fails
+  const t6 = await (
+    await _t.MaxValidation(client, { max_ten: "qweqweqweqweqweqweqweqwe" })
+  ).json();
+  assertEquals(t6, {
+    status: 400,
+    message: "Payload validation failed!",
+    data: {
+      errors: [
+        {
+          failed_field: "MaxTen",
+          message: "MaxTen should be less than 10 characters long",
+        },
+      ],
+    },
+  });
+
+  // Max validation passes
+  const t7 = await (await _t.MaxValidation(client, { max_ten: "qwe" })).json();
+  assertEquals(t7, response_ok);
 });
