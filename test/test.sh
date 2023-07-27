d268c199effd4bcae5d4aa1d2e2f1c433d5179c8
tompston <tompston@protonmail.com>
2023-07-27 18:11:58 +0300

    before publishing v0.10.0

--- test/test.sh
+++ test/test.sh
@@ -52,9 +52,9 @@ EXAMPLES=(
   # 'echo'
   # 'chi'
   # 'gin_with_modules'
-  # 'echo_with_modules'
-  # 'chi_with_modules'
-  'fiber_with_modules'
+  'echo_with_modules'
+  'chi_with_modules'
+  # 'fiber_with_modules'
 )
 
 # delete the build dir before generating the project
